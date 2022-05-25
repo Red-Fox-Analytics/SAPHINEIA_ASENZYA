148352724a1314c6ca8e6afdc0adf198b65f9ca2
Malik Redfox <malik@redfox.io>
2022-05-25 13:36:58 +0000

    more fields.

--- models/sa_sap_invoices.sql
+++ models/sa_sap_invoices.sql
@@ -7,18 +7,20 @@ invoices_data AS
 (
 	SELECT case when "Ownership" is null then 'Not Coded' else "Ownership" end "CUSTOMER_(PARENT)" 
 			, case when "Category" is null then 'Not Coded' else "Category" end CATEGORY
-			, "year" "YEAR", "Inv No" "INV_NO" , "Day of Week" "DAY_OF_WEEK"
-			, "item_wgt" "ITEM_WEIGHT", "Table Names-1"  "TABLE_NAMES_1"
+			, case when "year" is null then 'Not Coded' else "year" end "YEAR"
+			, case when "Inv No" is null then 'Not Coded' else "Inv No" end "INV_NO" , "Day of Week" "DAY_OF_WEEK"
+			, case when "item_wgt" is null then 'Not Coded' else "item_wgt" end "ITEM_WEIGHT", "Table Names-1"  "TABLE_NAMES_1"
 			, case when "Item Description" is null then 'Not Coded' else "Item Description" end "ITEM_DESCRIPTION"
 			, "Source" "SOURCE"
 			, case when "Segment" is null then 'Not Coded' else "Segment"  end "SEGMENT"
 			, "Table Names" "TABLE_NAMES", "Ownership ID" "CUSTOMER_ID(PARENT)"
-			, "date-month" "DATE_MONTH", "state" "STATE"	, "Cust No" "CUSTOMER_(CHILD)_ID", "Recipe ID" "RECIPE_ID" 
-			, "Customer Name" "CUSTOMER_(CHILD)", "Part Type"  "PART_TYPE", "File Paths" "FILE_PATHS", "Item_wgt class" "ITEM_WEIGHT_CLASS"
+			, "date-month" "DATE_MONTH", "state" "STATE"	
+			, case when "Cust No" is null then 'Not Coded' else "Cust No" end "CUSTOMER_(CHILD)_ID", "Recipe ID" "RECIPE_ID" 
+			, case when "Customer Name" is null then 'Not Coded' else "Customer Name" end "CUSTOMER_(CHILD)", "Part Type"  "PART_TYPE", "File Paths" "FILE_PATHS", "Item_wgt class" "ITEM_WEIGHT_CLASS"
 			, "Ship Qty" "SHIP QTY", "Date" "DATE", UOM "UOM"
 			, case when cast("Item No" as varchar) is null then 'Not Coded' else cast("Item No" as varchar) end  "ITEM_NO"
 			, "month" "MONTH"
-			, "location" "LOCATION"
+			, case when "location" is null then 'Not Coded' else "location" end "LOCATION"
 			, (Select NO_OF_ORDERS from invoices_count) NO_OF_ORDERS
 			
 			, case when "Volume Sales (LBs)" is null then 0 else "Volume Sales (LBs)" end "VOLUME_SALES_LB"
